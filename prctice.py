eeda4ee7bccd29109987b620ac998c70f2e3e1ae
iamadityasing <112313450+iamadityasing@users.noreply.github.com>
2023-01-27 06:16:21 +0000

    com

---
+++ prctice.py
@@ -0,0 +1,13 @@
+import re
+'''str1="emma luck numbers are 2514 7617 2319 4501"
+stringpattern=r"\d{4}"
+regex_pattern=re.compile(stringpattern)
+result=regex_pattern.findall(str1)
+print(result)'''
+# str="emma is a basketball player who was born ion june 17"
+# result=re.findall(r"\w{4}",str)
+# # print("Match.object:",result)
+# # print("Match value:",result.group())
+# print(result)
+
+#w is used for words and W is used for special characters
